b11d54ed2360ac4a6ceaf5d5ce73296a3204c322
lilgibs <lil.gibran01@gmail.com>
2025-06-08 22:57:38 +0700

    update: Sidebar adding logo, add hover menu

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,30 +2,38 @@
 import { LayoutGrid, User } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
+import logo from '@/assets/png/logo.png'
 
-export default function Sidebar({ isOpen, setIsOpen }: { isOpen: boolean, setIsOpen: React.Dispatch<React.SetStateAction<boolean>> }) {
+export default function Sidebar({ isOpen }: { isOpen: boolean }) {
 
   return (
     <div className={`sticky left-0 top-0 ${isOpen ? 'w-[300px]' : 'w-[72px]'} h-screen flex flex-col transition-all duration-300 ease-in-out`}>
-      <div className='h-[72px] flex items-center justify-center bg-white shadow-md'>
-        <p className='font-bold text-2xl'>LIL GIBRAN</p>
+      <div className='h-[72px] flex items-center justify-center gap-2 bg-white shadow-md'>
+        <Image
+          src={logo}
+          alt="LIL GIBRAN Logo"
+          width={42}
+          height={42}
+        />
+        <p className={`${isOpen ? 'block' : 'hidden'} text-nowrap overflow-hidden font-bold text-2xl`}>LIL GIBRAN</p>
       </div>
       <div className={`-z-10 flex-1 ${isOpen ? 'p-4' : 'py-4 px-2 items-center'} flex flex-col gap-4 bg-white font-semibold transition-all duration-300 ease-in-out`}>
         <p>MENU</p>
         <div className='flex flex-col gap-4'>
           <Link
-            className='flex gap-4'
+            className='p-2 flex gap-4 hover:bg-blue-50 rounded-md'
             href='/dashboard'
           >
             <LayoutGrid />
-            <span className={`${isOpen ? 'block' : 'hidden'} overflow-hidden`}>Dashboard</span>
+            <span className={`${isOpen ? 'block' : 'hidden'} text-nowrap overflow-hidden`}>Dashboard</span>
           </Link>
           <Link
-            className='flex gap-4'
+            className='p-2 flex gap-4 hover:bg-blue-50 rounded-md'
             href='/user'
           >
             <User />
-            <span className={`${isOpen ? 'block' : 'hidden'} overflow-hidden`}>User</span>
+            <span className={`${isOpen ? 'block' : 'hidden'} text-nowrap overflow-hidden`}>User</span>
           </Link>
         </div>
       </div>
